2e5f1ed0ca2e6342c7fca632d372d439473e6062
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-05-04 19:02:46 -0400

    1y2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import contactsJson from "./contacts.json";
 
+let firstFive = contactsJson.slice(0, 5);
 function App() {
+  const [contacts, setContacts] = React.useState(firstFive);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <th>picture</th>
+          <th>name</th>
+          <th>popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        {contacts.map((contact) => {
+          return (
+            <tr>
+              <td>
+                <img src={contact.pictureUrl} height="50" width="40" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar && "🏆"}</td>
+              <td>{contact.wonEmmy && "🏆"}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
 
 export default App;
+
